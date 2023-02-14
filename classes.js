10d85ea53798123bc14ab884a836b79fb2cdd3bd
Lucas Tymen <lucas.tymen@gmail.com>
2023-02-15 00:03:52 +0100

    Introduction to Classes

--- classes.js
+++ classes.js
@@ -1,4 +1,20 @@
+class Dog {
+  constructor(name) {
+    this._name = name;
+    this._behavior = 0;
+  }
 
+  get name() {
+    return this._name;
+  }
+  get behavior() {
+    return this._behavior;
+  }
+
+  incrementBehavior() {
+    this._behavior ++;
+  }
+}
 
 // principles :
 /*
