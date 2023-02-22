40bd1d2580f01871e812dc8c1a40c5250195c50f
Lucas Tymen <lucas.tymen@gmail.com>
2023-02-23 00:39:42 +0100

    NodeJsInitiationExrcz2

--- NodeInitiationExercise.js
+++ NodeInitiationExercise.js
@@ -1,3 +1,15 @@
+/*
+Code Challenges
+Coding question
+
+Complete the shape-area.js module. Your module must declare and export two functions with the following signatures:
+
+    circleArea(radiusLength).
+    squareArea(sideLength).
+
+You may create these as either named functions or anonymous function expressions.
+*/
+
 /* shape-area.js */
 const PI = Math.PI;
 
